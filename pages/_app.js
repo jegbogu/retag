443386c2e177881b6441c3541f3b9a2598059982
jegbogu <jegbogu@gmail.com>
2023-10-04 13:49:33 +0100

    protecting routes

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,17 @@
 import Layout from '@/components/layout/layout'
+import { SessionProvider } from 'next-auth/react'
 import { UserContextProvider } from '@/store/userContext'
 import '@/styles/globals.css'
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps:{session,...pageProps}, }) {
   return (
+    <SessionProvider session={session}>
     <UserContextProvider>
       <Layout>
         <Component {...pageProps} />
       </Layout>
     </UserContextProvider>
+    </SessionProvider>
   )
 
 
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,9 +1,20 @@
+import { useSession } from "next-auth/react"
+
 function Dashboard(){
+const{data:session, status} = useSession()
+if(status==='loading'){
+    return <p>Loading...</p>
+}else if(status==='unauthenticated'){
+    return <p>Unauthourised</p>
+}else{
     return(
         <div>
             <h1>Dashboard</h1>
         </div>
     )
+}
+
+   
 }
 
 export default Dashboard
